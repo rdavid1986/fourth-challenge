b23140078cd1300fdb54f218381ded56db59bbd0
David Rodriguez <rdavid1986@outlook.com>
2023-04-26 13:41:46 -0300

    corrected

--- firstChallenge.js
+++ firstChallenge.js
@@ -1,44 +1,41 @@
+class ProductManager {
+    static lastId = 0;
+    static products = [];
 
-const products = [];
-class ProductManager{
-    static lastId = -1;
     constructor(title, description, price, thumbnail, code, stock) {
-        
         this.title = title;
         this.description = description;
         this.price = price;
         this.thumbnail = thumbnail;
         this.code = code;
         this.stock = stock;
-        this.id = 0;
-        
+        this.id = 1;
     }
-    static addProduct(product){
-        if(products.some(existingProduct => existingProduct.code === product.code)){
+
+    static addProduct(product) {
+        if (ProductManager.products.some(existingProduct => existingProduct.code === product.code)) {
             console.log(`ERROR: this product code : ${product.code} already exists in products`);
-            
-        } else if (product.title === "" || product.description === "" || product.price === "" || product.thumbnail === "" || product.code === "" || product.stock === ""){
-                console.log("All fields are required");
+        } else if (product.title === "" || product.description === "" || product.price === "" || product.thumbnail === "" || product.code === "" || product.stock === "") {
+            console.log("All fields are required");
         } else {
             product.id = ++ProductManager.lastId;
-            products.push(product);
+            ProductManager.products.push(product);
         }
-    } 
-}
-function getProducts(){
-    console.log(`Array products contains ${products.length} products` )
-    console.log(products)
-}
-function getProductById(id){
-    const productById = products.find(product => product.id === id);
-    if(productById) {
-        console.log(`this is the product that was searched by id "${productById.title}"`);
-    } else {
+    }
 
-        console.log(`Product not found with that ID`);
+    static getProducts() {
+        console.log(ProductManager.products);
+    }
+
+    static getProductById(id) {
+        const productById = ProductManager.products.find(product => product.id === id);
+        if (productById) {
+            console.log(`This is the product that was searched by id "${productById.title}"`);
+        } else {
+            console.log(`Product not found with that ID`);
+        }
     }
 }
-getProducts()
 
 const product1 = new ProductManager("Producto prueba 1", "Este es un producto prueba", 200, "sin imagen", "abc123", 25);
 const product2 = new ProductManager("Producto prueba 2", "Este es un producto prueba", 200, "sin imagen", "abc123", 25);//Repeated key product
@@ -50,8 +47,7 @@ ProductManager.addProduct(product2);
 ProductManager.addProduct(product3);
 ProductManager.addProduct(product4);
 
-getProducts();
+ProductManager.getProducts();
 
-getProductById(1);
-getProductById(19); //This product does not exist
-
+ProductManager.getProductById(1);
+ProductManager.getProductById(19); //This product does not exist
