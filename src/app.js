7bfa3060018485c98c06e8c5c6425ae47ef9d684
David Rodriguez <rdavid1986@outlook.com>
2023-05-15 21:20:15 -0300

    third challenge ready with qury limit ready

--- src/app.js
+++ src/app.js
@@ -14,14 +14,18 @@ app.get('/', (req, res)=> {
 //rute /products type app.get calls the productManager class method getProducts();
 app.get('/products', async (req, res) => {
     const products = await productManager.getProducts();
+    console.log("hola")
     res.send(products)
 });
-/* app.get('/products', async (req, res) => {
-    const limit = req.query.limit || 5;
-    const products = await productManager.getProducts(limit);
-    res.send("estos son los products de limit",products)
-    console.log(limit)
-}); */
+//rute /product?limit get limited product list default in 5 products or the amount of you choose 
+app.get('/product', async (req, res) => {
+    const limit = parseInt(req.query.limit) || 5;
+    const products = await productManager.getProducts();
+    const productsLimit = products.slice(0,limit);
+    console.log("chau")
+    res.send(JSON.stringify(productsLimit))
+});
+
 //rute /products/:id get products by id in products.json of ProductsManager class
 app.get('/products/:id', async  (req, res) => {
     const id = req.params.id
